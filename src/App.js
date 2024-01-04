7fc57b475b549cbcb406e9a1609a5475c8242a8c
Devesh <deveshdevroop@gmail.com>
2024-01-04 03:25:17 +0200

    robofriends project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CardList from './CardList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Searchbox from './Searchbox';
+import './App.css';
+import Scroll from './Scroll'
+import ErrorBoundary from './ErrorBoundary';
+
+
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>{
+            return response.json();
+        })
+        .then(users=> {this.setState({robots: users})});
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({searchfield: event.target.value})
+        
+        }
+        
+    
+    render(){
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        if(this.state.robots.length === 0){
+            return <h1 className='tc'>Loading</h1>
+        }
+        else{
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>Robofriends</h1>
+                    <Searchbox searchChange = {this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots = {filteredRobots}/>
+                        </ErrorBoundary>
+                        
+                    </Scroll>
+                    
+                </div>
+                
+            );
+        }
+        
+    }
+    
 }
 
 export default App;
